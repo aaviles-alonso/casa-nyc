565acdd48a8d4b01bc2eaa404e108a5dd5701bd3
Alexa <alexaa6415@gmail.com>
2023-04-07 14:06:45 -0400

    uploaded hero image for website

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,14 @@
+import hero from "../../assets/icons/hero.jpeg";
+
 // stylesheet
 import "./Home.scss";
 
 export default function Home() {
-
-}
+    return (
+        <>
+            <div className="hero">
+                <img src={hero} alt="cover photo" className="hero__image"></img>
+            </div>
+        </>
+    )
+};
