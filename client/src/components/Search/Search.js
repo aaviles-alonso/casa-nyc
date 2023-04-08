6415fb8873a1a5871e6071f17f85c9a5b103ea99
Alexa <alexaa6415@gmail.com>
2023-04-08 14:19:49 -0400

    Added search bar

---
+++ client/src/components/Search/Search.js
@@ -0,0 +1,15 @@
+import "./Search.scss";
+
+const Search = () => {
+    return (
+        <form>
+            <input
+                type="text"
+                placeholder="Search"
+                className="search__text"
+            >
+            </input>
+        </form>
+    );
+};
+export default Search;
--- client/src/pages/Organizations/Organizations.js
+++ client/src/pages/Organizations/Organizations.js
@@ -1,6 +1,23 @@
 // stylesheet
 import "./Organizations.scss";
+import "../../styles/global.scss";
+
+// components
+import Search from "../../components/Search/Search";
 
 export default function Organizations() {
+    return (
+        <>
+            <div className="organization">
+                <h1>ORGANIZATIONS</h1>
+                <div className="organization__search">
+                    <Search />
+                </div>
+                <div className="organization__container">
+                    <h3>Organization Title</h3>
 
-}
+                </div>
+            </div>
+        </>
+    );
+};
